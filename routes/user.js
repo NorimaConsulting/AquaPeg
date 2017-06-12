577b8f6fd47ea066121f9494e11b761e401e671f
Casey Forsyth <casey.forsyth@norimaconsulting.com>
2017-06-12 09:56:51 -0500

    Removed an old import

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,5 @@
 const bluebird = require('bluebird');
 const crypto = bluebird.promisifyAll(require('crypto'));
-const nodemailer = require('nodemailer');
 const passport = require('passport');
 const User = require('../models/User');
 const emailController = require('../controllers/emailController')
